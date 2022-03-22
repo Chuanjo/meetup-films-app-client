f700a5da0a7ff19ba0ef25e72fbf8e2ea03cc234
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-03-22 09:53:45 +0100

    meetup

--- src/pages/MeetUpList.jsx
+++ src/pages/MeetUpList.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function MeetUpList() {
+function MeetUpList(props) {
+ 
+  const[allComments, setAllComments] = useState([]);
+  const { id } =props.params
+  
+  useEffect (() => {
+
+  }, [])
+
+
   return (
     <div>
 
     <h1>MeetUpList</h1>
+    
+   
     <h1>AddMeetUp Component</h1>
     <h1>All Meet up List</h1>
 
