ee4a117a9f0d2ada1f474069c038f429d45dd834
Chuanjo <chuanjo@hotmail.com>
2022-03-24 13:37:46 +0100

    user profile y meetup list por id

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
+import { myUserProfileService } from "../services/user.services"
 import { meetUpListUserIdService } from "../services/meetUpList.services"
 
 function Profile() {
-  const [ meetUpListUserId, setMeetUpListUserId ] = useState(null)
+  const [ userInfermation, setUserInformation ] = useState(null)
+  const [ meetUpList, setMeetUpList ] = useState(null)
 
   const { id } = useParams();
 
   const navigate = useNavigate();
   
-  // useEffect(() => {
-  //   getMeetUpLisUserId();
-  // }, []);
+  useEffect(() => {
+    getInformation();
+  }, []);
   
-  // const getMeetUpLisUserId = async () => {
-  //   try {
-  //     const response = await meetUpListUserIdService()
-  //     setMeetUpListUserId(response.data)
-      
-  //   } 
-  //   catch(error) 
-  //   {
-  //     if (error.response.status === 401) {
-  //       navigate("/login");
-  //     } else {
-  //       navigate("/error");
-  //     }
-  //   }
-  // }
+  const getInformation = async () => {
+    try {
+      const response = await myUserProfileService()
+      setUserInformation(response.data)
+      const response2 = await meetUpListUserIdService()
+      setMeetUpList(response2.data)
+    } 
+    catch(error) 
+    {
+      if (error.response.status === 401) {
+        navigate("/login");
+      } else {
+        navigate("/error");
+      }
+    }
+  }
 
-  // if (!meetUpListUserId) {
-  //   return <h3>...Loading</h3>
-  // }
+  if (!userInfermation && !meetUpList) {
+    return <h3>...Loading</h3>
+  }
 
   return (
     <div>
